7e2283576f101f5a8f86e3ee5ebf330c22348d73
Souissi Alaa <54039072+SouissiAlaa12@users.noreply.github.com>
2020-12-03 22:48:18 +0100

    Add files via upload

---
+++ Page 2/Page 2/register.js
@@ -0,0 +1,4 @@
+document.getElementById('send').addEventListener('submit', function (event) {
+    event.preventDefault()
+    window.location = "../../Page 3/Page 3/login.html"
+})
---
+++ Page 3/Page 3/login.js
@@ -0,0 +1,4 @@
+document.getElementById('send').addEventListener('submit', function (event) {
+    event.preventDefault()
+    window.location = "../../Page 1/Page 1/page1.html"
+})
